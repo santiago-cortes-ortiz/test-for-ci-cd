01b3e2378178903bd40ce31376a47519b7c304ef
jeiss <jeissoncortes02@outlook.com>
2022-10-05 19:42:05 -0500

    prueba

---
+++ src/main/java/com/example/testforcicd/TestForCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.testforcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestForCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestForCiCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testforcicd/TestForCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testforcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestForCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
