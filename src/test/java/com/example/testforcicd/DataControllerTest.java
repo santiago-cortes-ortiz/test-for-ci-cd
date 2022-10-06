c52120ba54e170bbe30c563a331882ccd3a0ad18
jeiss <jeissoncortes02@outlook.com>
2022-10-05 19:53:12 -0500

    yml

--- src/test/java/com/example/testforcicd/DataControllerTest.java
+++ src/test/java/com/example/testforcicd/DataControllerTest.java
@@ -14,7 +14,7 @@ class DataControllerTest {
 
     @Test
     void health() {
-        assertEquals( "HEALTH CHECK OK!", dataController.healthCheck());
+        assertEquals( "HEALTH CHECK OK", dataController.healthCheck());
     }
 
     @Test
