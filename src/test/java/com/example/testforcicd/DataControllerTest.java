38fad3b0fb3fa69c1ab8ae69d5f4f3f13bcb2005
jeiss <jeissoncortes02@outlook.com>
2022-10-06 17:28:22 -0500

    sonar

--- src/test/java/com/example/testforcicd/DataControllerTest.java
+++ src/test/java/com/example/testforcicd/DataControllerTest.java
@@ -14,7 +14,7 @@ class DataControllerTest {
 
     @Test
     void health() {
-        assertEquals( "HEALTH CHECK O!", dataController.healthCheck());
+        assertEquals( "HEALTH CHECK OK!", dataController.healthCheck());
     }
 
     @Test
